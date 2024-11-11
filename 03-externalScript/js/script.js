1daa7015e91f1353b364f1100ae9078450f40ae6
SivasankarThalavai <s2.thalavai@gmail.com>
2024-11-11 09:04:24 +0530

    external css & js scripts on html file

---
+++ 03-externalScript/js/script.js
@@ -0,0 +1,5 @@
+let today_Date = new Date();
+
+function showHiddenText() {
+    document.getElementById("demo").innerHTML = "Hello World <h3>Today's date is " + today_Date + "</h3> ";
+}
--- hello-world/externalScript/scripts/code.js
+++
@@ -1,3 +0,0 @@
-function showHiddenText() {
-    document.getElementById("demo").innerHTML = "Hello World";
-}
